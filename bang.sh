e7982d290831e498d522ebd8e173dbb40f7e020e
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-06-13 23:14:53 +0000

    added bang script

---
+++ bang.sh
@@ -0,0 +1,25 @@
+echo "Pick One of those 1. Packages | 2. Firefox | 3. .dots | 0. All"
+read -p "Lets Do it: " option
+
+case "$option" in
+0)
+    bash package.sh
+    bash firefox.sh
+    bash dots.sh
+    ;;
+1)
+    bash package.sh
+    ;;
+2)
+    bash firefox.sh
+    ;;
+3)
+    bash dots.sh
+    ;;
+*)
+    echo "Invalid choice"
+    exit 0
+    ;;
+esac
+
+swaybg -i $HOME/bang/Standing.png -m fill &
