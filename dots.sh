01ca8455031490fb1b56eaab040f7a5dbbc66181
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-06-20 11:16:14 +0600

    added uosc mpv

--- dots.sh
+++ dots.sh
@@ -2,6 +2,7 @@
 declare -A dotfiles=(
     ["$HOME/bang/dots/mimeapps.list"]="$HOME/.config/mimeapps.list"
     ["$HOME/bang/dots/mango"]="$HOME/.config/mango"
+    ["$HOME/bang/dots/mpv"]="$HOME/.config/mpv"
     ["$HOME/bang/dots/foot"]="$HOME/.config/foot"
     ["$HOME/bang/dots/waybar"]="$HOME/.config/waybar"
     ["$HOME/bang/dots/.bashrc"]="$HOME/.bashrc"
