a1976f189497ea5f2b2da90981574b1e0985b0a9
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-06-28 02:05:04 +0600

    fixed packages

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 bash $HOME/bang/reflactor.sh
 
 # package install section
-sudo pacman -S git imlib2 ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts base-devel github-cli lazygit fzf zoxide starship
+sudo pacman -S git imlib2 ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts base-devel github-cli lazygit fzf zoxide starship swayimg
 sudo pacman -R ex-vi-compat vim
 
 # self package-section
@@ -10,24 +10,14 @@ mkdir $HOME/pkg
 cd $HOME/pkg
 
 url="https://github.com/HimadriChakra12"
-git clone $url/rsxiv    sxiv
 git clone $url/swat	    swat
-git clone $url/px	    px
 git clone $url/fetch	fetch
 git clone $url/dtop	    dtop
 git clone $url/whot	    whot
 git clone $url/determinant	    det
 
-cd sxiv
-bash install.sh
-cd ..
-
 cd swat
 bash install.sh
-cd ..
-
-cd px
-sudo make install
 cd ..
 
 cd fetch
@@ -43,11 +33,6 @@ sudo make install
 cd ..
 
 cd whot
-sudo make install
-cd ..
-
-git clone https://github.com/resslr/aurc.git
-cd aurc
 sudo make install
 cd ..
 
