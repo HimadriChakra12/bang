ef6a7482eab1cc6ea09736ad9f76921b85f08604
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-07-11 20:00:27 +0600

    pkg's

--- make/pkg.mk
+++ make/pkg.mk
@@ -1,5 +1,5 @@
 PKG         = $(HOME)/pkg
-PKGIN       = git imlib2 ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts base-devel github-cli lazygit fzf zoxide starship swayimg redshift
+PKGIN       = git imlib2 ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts base-devel github-cli lazygit fzf zoxide starship swayimg gammashift firefox
 PKGRM       = ex-vi-compat vim
 URL         = https://github.com/HimadriChakra12
 MAKEREPOS   = fetch dtop whot det sxbv
